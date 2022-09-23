8ad1277d7522112d002195eb43ccce17c0f03884
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2022-09-23 14:24:23 +0800

    解释并输出产品评价分词结果

---
+++ Makefile
@@ -0,0 +1,60 @@
+BINARY="diana"
+VERSION="2022092301"
+
+default: help
+
+help: ## 显示帮助信息
+	@echo 'usage: make [targets]'
+	@echo ''
+	@echo 'targets:'
+	@egrep '^(.+)\:\ .*##\ (.+)' ${MAKEFILE_LIST} | sed 's/:.*##/#/' | column -t -c 2 -s '#'
+
+build: ## 构建应用程序
+	go build -o bin/${BINARY}-${VERSION}-local facial/cmd/facial
+	go build -o bin/${BINARY}-api-${VERSION}-local facial/cmd/api
+	go build -o bin/${BINARY}-bot-${VERSION}-local facial/cmd/bot
+	@echo '本地应用程序构建完毕' 
+
+build-linux: ## 构建Linux应用程序
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/${BINARY}-${VERSION}-linux-amd64 facial/cmd/facial
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/${BINARY}-api-${VERSION}-linux-amd64 facial/cmd/api
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/${BINARY}-bot-${VERSION}-linux-amd64 facial/cmd/bot
+	@echo 'linux应用程序构建完毕'
+
+build-mac: ## 构建mac应用程序
+	CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/${BINARY}-${VERSION}-darwin-amd64 facial/cmd/facial
+	CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/${BINARY}-api-${VERSION}-darwin-amd64 facial/cmd/api
+	CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/${BINARY}-bot-${VERSION}-darwin-amd64 facial/cmd/bot
+	@echo 'mac应用程序构建完毕'
+
+build-windows: ## 构建windows应用程序
+	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/${BINARY}-${VERSION}-windows-amd64.exe facial/cmd/facial
+	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/${BINARY}-api-${VERSION}-windows-amd64.exe facial/cmd/api
+	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/${BINARY}-bot-${VERSION}-windows-amd64.exe facial/cmd/bot
+	@echo 'windows应用程序构建完毕'
+
+deploy-data: clean build-linux ## 发布到生产环境
+	scp bin/${BINARY}-${VERSION}-linux-amd64 FacialServer:/home/neo/apps/facial/bin/facial
+	@echo '发布完毕'
+
+deploy-api: clean build-linux ## 发布服务到生产环境
+	scp bin/${BINARY}-api-${VERSION}-linux-amd64 FacialServer:/home/neo/apps/facial/bin/facial_api
+	@echo '发布完毕'
+
+deploy-bot: clean build-linux ## 发布服务到生产环境
+	scp bin/${BINARY}-bot-${VERSION}-linux-amd64 FacialServer:/home/neo/apps/facial/bin/facial_bot
+	@echo '发布完毕'
+
+clean: ## 清除构建文件
+	rm bin/*
+	@echo '过去构建文件清理完毕'
+
+doc: ## 生成 godocs 文档并开启本地文档服务
+	godoc -http=:8085 -index
+
+db-migrate: ## 运行数据库迁移命令
+	@echo '还没实现'
+
+swagger-ui: ## 运行本地 swagger-ui
+	@echo '还没实现'
+
---
+++ cmd/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/go-ego/gse"
+	"github.com/xuri/excelize/v2"
+)
+
+type Evaluation struct {
+	Date              string // 日期
+	PlatformName      string // 平台
+	ProductName       string // 产品名称
+	EvaluationContent string // 好评/差评
+	EvaluationComment string // 评价内容
+}
+
+func main() {
+	orderFile := "/Users/sheldon/8月全平台顾客评价汇总表0905.xlsx"
+	f, err := excelize.OpenFile(orderFile)
+	if err != nil {
+		log.Fatalf("Parse Excel Data File Error: %v", err)
+	}
+
+	var sheetName string
+	index := 0
+
+	for _, name := range f.GetSheetMap() {
+		if index > 0 {
+			break
+		}
+		sheetName = name
+		index++
+	}
+
+	var evaluationList []Evaluation
+
+	rows, _ := f.GetRows(sheetName)
+	for rowIndex, row := range rows {
+		if rowIndex > 0 {
+			evaluation := Evaluation{}
+
+			for colIndex, colCell := range row {
+				colCell = strings.TrimSpace(colCell)
+
+				switch colIndex {
+				case 0:
+					// 日期
+					evaluation.Date = colCell
+				case 1:
+					// 平台
+					evaluation.PlatformName = colCell
+				case 2:
+					// 产品名称
+					evaluation.ProductName = colCell
+				case 3:
+					// 好评/差评
+					evaluation.EvaluationContent = colCell
+				case 4:
+					// 评价内容
+					evaluation.EvaluationComment = colCell
+				}
+			}
+
+			evaluationList = append(evaluationList, evaluation)
+		}
+	}
+
+	result := make(map[string]map[string]map[string]int)
+
+	var seg gse.Segmenter
+
+	seg.LoadDict()
+	new, _ := gse.New("zh,testdata/test_dict3.txt", "alpha")
+
+	for _, evaluation := range evaluationList {
+		platformResult, ok := result[evaluation.ProductName]
+		if !ok {
+			platformResult = make(map[string]map[string]int)
+		}
+
+		result[evaluation.ProductName] = platformResult
+
+		platform := fmt.Sprintf("%s-%s", evaluation.PlatformName, evaluation.EvaluationContent)
+
+		contentResult, ok := platformResult[platform]
+		if !ok {
+			contentResult = make(map[string]int)
+		}
+
+		platformResult[platform] = contentResult
+
+		hmm := new.Cut(evaluation.EvaluationComment, true)
+		for _, c := range hmm {
+			countResult, ok := contentResult[c]
+			if !ok {
+				contentResult[c] = 1
+			} else {
+				contentResult[c] = countResult + 1
+			}
+
+		}
+	}
+
+	for productName, platformResult := range result {
+		f := excelize.NewFile()
+
+		for platformName, contentResult := range platformResult {
+			index := f.NewSheet(platformName)
+			f.SetActiveSheet(index)
+
+			loc := 2
+
+			f.SetCellValue(platformName, "A1", "关键词")
+			f.SetCellValue(platformName, "B1", "次数")
+
+			for content, count := range contentResult {
+				locA := fmt.Sprintf("A%d", loc)
+				locB := fmt.Sprintf("B%d", loc)
+
+				f.SetCellValue(platformName, locA, content)
+				f.SetCellValue(platformName, locB, count)
+
+				loc++
+			}
+		}
+
+		excelFileName := fmt.Sprintf("%s.xlsx", productName)
+		if err := f.SaveAs(excelFileName); err != nil {
+			fmt.Printf("save file %s, err: %v", excelFileName, err)
+		}
+	}
+}
